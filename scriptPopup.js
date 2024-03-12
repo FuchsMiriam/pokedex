ef12ff481ad3f953fe44d8d4f14cce9d49a3ffd7
Miriam Fuchs <fuchsmiriam@yahoo.com>
2024-03-12 17:25:25 +0100

    changed popup

--- scriptPopup.js
+++ scriptPopup.js
@@ -1,3 +1,21 @@
-
+function openTab(tabName) {
+    // Alle Tab-Inhalte ausblenden
+    var tabcontents = document.getElementsByClassName("tabcontent");
+    for (var i = 0; i < tabcontents.length; i++) {
+        tabcontents[i].style.display = "none";
+    }
+    
+    // Deaktiviere alle Tab-Buttons
+    var tablinks = document.getElementsByClassName("tablink");
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    
+    // Zeige den ausgewählten Tab-Inhalt
+    document.getElementById(tabName).style.display = "block";
+    
+    // Aktiviere den ausgewählten Tab-Button
+    event.currentTarget.classList.add("active");
+}
 
 
