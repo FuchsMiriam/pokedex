5b61b03e8ad8f865639d3fb6e62f07d7c0050ba0
Miriam Fuchs <fuchsmiriam@yahoo.com>
2024-03-13 18:50:33 +0100

    header implemented

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 let currentPokemon;
 
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html"); // "includes/header.html"
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
+
 async function loadPokemon() {
     let url = 'https://pokeapi.co/api/v2/pokemon/togepi';
     let response = await fetch(url);
